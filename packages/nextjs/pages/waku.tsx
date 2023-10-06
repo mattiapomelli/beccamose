a3580f24497037e6186b1211b58a70f0d2983aee
mattiapomelli <mattiapomelli@gmail.com>
2023-10-06 21:41:33 +0200

    Send geolocation in message

--- packages/nextjs/pages/waku.tsx
+++ packages/nextjs/pages/waku.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { useGeolocated } from "react-geolocated";
 import { Button } from "~~/components/ui/Button";
 import { useMessages, useSendMessage } from "~~/sdk";
 
@@ -6,10 +7,22 @@ const WakuPage: NextPage = () => {
   const { sendMessage } = useSendMessage();
   const { messages } = useMessages();
 
+  const { coords } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    userDecisionTimeout: 5000,
+  });
+
   console.log("Messages: ", messages);
 
   return (
     <div>
+      <div>Lat: {coords?.latitude}</div>
+      <div>
+        Long:
+        {coords?.longitude}
+      </div>
       <Button onClick={() => sendMessage()}>Send</Button>
     </div>
   );
--- packages/nextjs/sdk/hooks/useSendMessage.ts
+++ packages/nextjs/sdk/hooks/useSendMessage.ts
@@ -1,21 +1,33 @@
 import { LocationMessage } from "../constants";
 import { LightNode } from "@waku/interfaces";
 import { useContentPair, useLightPush, useWaku } from "@waku/react";
+import { useGeolocated } from "react-geolocated";
 
 export const useSendMessage = () => {
   const { node } = useWaku<LightNode>();
   const { encoder } = useContentPair();
   const { push: onPush } = useLightPush({ node, encoder });
 
+  const { coords } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+  });
+
   const sendMessage = async () => {
-    if (!onPush) return;
+    if (!onPush || !coords) return;
 
     const timestamp = new Date();
     // Create a new message object
     const protoMessage = LocationMessage.create({
       timestamp: Date.now(),
       sender: "Alice",
-      message: "Beccamose!",
+      message: JSON.stringify({
+        lat: coords.latitude,
+        long: coords.longitude,
+      }),
+      nonce: Math.floor(Math.random() * 1000000000000),
     });
     // Serialise the message using Protobuf
     const payload = LocationMessage.encode(protoMessage).finish();
