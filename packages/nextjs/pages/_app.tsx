e6d19a238b18eeaf219bb278d643706853ce4c8c
Stefano Faieta <9802152+stefanofa@users.noreply.github.com>
2023-10-07 15:51:43 +0200

    Add tanstack query

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -1,14 +1,16 @@
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { enrTree, wakuDnsDiscovery } from "@waku/dns-discovery";
 import { Protocols } from "@waku/interfaces";
 import { wakuPeerExchangeDiscovery } from "@waku/peer-exchange";
 import { ContentPairProvider, LightNodeProvider } from "@waku/react";
 import NextNProgress from "nextjs-progressbar";
-import { ErrorBoundary } from "react-error-boundary";
 import { Toaster } from "react-hot-toast";
 import { useDarkMode } from "usehooks-ts";
 import { WagmiConfig } from "wagmi";
@@ -23,7 +25,25 @@ import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 import { appChains } from "~~/services/web3/wagmiConnectors";
 import "~~/styles/globals.css";
 
-const Main = ({ children }: { children: ReactNode }) => {
+const queryClient = new QueryClient();
+const ReactQueryDevtoolsProduction = dynamic(
+  () =>
+    // eslint-disable-next-line import/extensions
+    import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(d => ({
+      default: d.ReactQueryDevtools,
+    })),
+  { ssr: false },
+);
+
+declare global {
+  interface Window {
+    toggleDevtools: () => void;
+  }
+}
+
+const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
+  const price = useNativeCurrencyPrice();
+  const setNativeCurrencyPrice = useGlobalState(state => state.setNativeCurrencyPrice);
   const { isDarkMode } = useDarkMode();
   const [isDarkTheme, setIsDarkTheme] = useState(true);
 
@@ -31,56 +51,10 @@ const Main = ({ children }: { children: ReactNode }) => {
     setIsDarkTheme(isDarkMode);
   }, [isDarkMode]);
 
-  return (
-    <LightNodeProvider
-      // options={{ defaultBootstrap: true }}
-      options={{
-        defaultBootstrap: false,
-        libp2p: {
-          peerDiscovery: [
-            wakuDnsDiscovery([enrTree["PROD"]], { lightPush: 6, filter: 6, store: 6 }),
-            wakuPeerExchangeDiscovery(),
-          ],
-        },
-      }}
-      protocols={[Protocols.Filter, Protocols.LightPush, Protocols.Store]}
-    >
-      <ContentPairProvider contentTopic={CONTENT_TOPIC}>
-        <NextNProgress />
-        <RainbowKitProvider
-          chains={appChains.chains}
-          avatar={BlockieAvatar}
-          theme={isDarkTheme ? darkTheme() : lightTheme()}
-        >
-          <DerivedAccountProvider>
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <main className="relative flex flex-col flex-1">{children}</main>
-              <Footer />
-            </div>
-            <Toaster />
-          </DerivedAccountProvider>
-        </RainbowKitProvider>
-      </ContentPairProvider>
-    </LightNodeProvider>
-  );
-};
-
-function fallbackRender({ error, resetErrorBoundary }: any) {
-  resetErrorBoundary();
-
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-    </div>
-  );
-}
-
-const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
-  const price = useNativeCurrencyPrice();
-  const setNativeCurrencyPrice = useGlobalState(state => state.setNativeCurrencyPrice);
-  // This variable is required for initial client side rendering of correct theme for RainbowKit
+  const [showDevtools, setShowDevtools] = useState(false);
+  useEffect(() => {
+    window.toggleDevtools = () => setShowDevtools(old => !old);
+  }, []);
 
   const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract";
   const apolloClient = new ApolloClient({
@@ -95,15 +69,47 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
   }, [setNativeCurrencyPrice, price]);
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <WagmiConfig config={wagmiConfig}>
-        <ErrorBoundary fallbackRender={fallbackRender} onError={() => console.log(">>>> ERROR <<<<")}>
-          <Main>
-            <Component {...pageProps} />
-          </Main>
-        </ErrorBoundary>
-      </WagmiConfig>
-    </ApolloProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {showDevtools && <ReactQueryDevtoolsProduction />}
+      <ApolloProvider client={apolloClient}>
+        <WagmiConfig config={wagmiConfig}>
+          <LightNodeProvider
+            // options={{ defaultBootstrap: true }}
+            options={{
+              defaultBootstrap: false,
+              libp2p: {
+                peerDiscovery: [
+                  wakuDnsDiscovery([enrTree["PROD"]], { lightPush: 6, filter: 6, store: 6 }),
+                  wakuPeerExchangeDiscovery(),
+                ],
+              },
+            }}
+            protocols={[Protocols.Filter, Protocols.LightPush, Protocols.Store]}
+          >
+            <ContentPairProvider contentTopic={CONTENT_TOPIC}>
+              <NextNProgress />
+              <RainbowKitProvider
+                chains={appChains.chains}
+                avatar={BlockieAvatar}
+                theme={isDarkTheme ? darkTheme() : lightTheme()}
+              >
+                <DerivedAccountProvider>
+                  <div className="flex flex-col min-h-screen">
+                    <Header />
+                    <main className="relative flex flex-col flex-1">
+                      <Component {...pageProps} />
+                    </main>
+                    <Footer />
+                  </div>
+                  <Toaster />
+                </DerivedAccountProvider>
+              </RainbowKitProvider>
+            </ContentPairProvider>
+          </LightNodeProvider>
+        </WagmiConfig>
+      </ApolloProvider>
+    </QueryClientProvider>
   );
 };
 
