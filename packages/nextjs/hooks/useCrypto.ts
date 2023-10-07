193b803ae5784fb6ed5615fc0747cf148a076d29
Stefano Faieta <9802152+stefanofa@users.noreply.github.com>
2023-10-07 11:09:59 +0200

    Add crypto module

---
+++ packages/nextjs/hooks/useCrypto.ts
@@ -0,0 +1,30 @@
+import EthCrypto from "eth-crypto";
+import { hashMessage } from "viem";
+import { recoverPublicKey } from "viem";
+import { useBurnerWallet } from "~~/hooks/scaffold-eth";
+
+export const useDecrypt = () => {
+  const { burnerSk } = useBurnerWallet();
+
+  const decryptMessage = async (encryptedMessage: any) => {
+    const decryptedMessage = await EthCrypto.decryptWithPrivateKey(burnerSk, encryptedMessage);
+    return decryptedMessage;
+  };
+
+  return { decryptMessage };
+};
+
+export const recoverPublicKeyFromMessageAndSignature = async (message: string, signature: `0x${string}`) => {
+  return recoverPublicKey({ hash: hashMessage(message), signature });
+};
+
+export const generateEncryptionClient = (publicKey: `0x${string}`) => {
+  // remove leading 0x04
+  const strippedPublicKey = publicKey.replace(/^0x04/, "");
+
+  const encryptMessage = async (message: string) => {
+    return EthCrypto.encryptWithPublicKey(strippedPublicKey, message);
+  };
+
+  return { encryptMessage };
+};
