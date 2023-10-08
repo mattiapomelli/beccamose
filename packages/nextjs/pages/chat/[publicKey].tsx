a4c7808802116d2f733019238f4908364142ff7c
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-08 06:36:06 +0200

    Remove button check

--- packages/nextjs/pages/chat/[publicKey].tsx
+++ packages/nextjs/pages/chat/[publicKey].tsx
@@ -15,32 +15,6 @@ const Map = dynamic(() => import("../../components/my-map/my-map"), {
   ssr: false,
 });
 
-function haversineDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
-  const toRadians = (degree: number): number => degree * (Math.PI / 180);
-
-  // Convert degrees to radians
-  lat1 = toRadians(lat1);
-  lon1 = toRadians(lon1);
-  lat2 = toRadians(lat2);
-  lon2 = toRadians(lon2);
-
-  // Difference in coordinates
-  const dlat = lat2 - lat1;
-  const dlon = lon2 - lon1;
-
-  // Haversine formula
-  const a = Math.sin(dlat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2;
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  // Radius of Earth in kilometers
-  const R = 6371.0;
-
-  // Distance in kilometers
-  const distance = R * c;
-
-  return distance;
-}
-
 const InvitePage: NextPage = () => {
   const { address } = useAccount();
   const router = useRouter();
@@ -74,14 +48,6 @@ const InvitePage: NextPage = () => {
     await mintNFT();
   };
 
-  const distanceBetweenPointsInMeters =
-    haversineDistance(
-      coords?.latitude || 0,
-      coords?.longitude || 0,
-      otherCoords?.latitude || 0,
-      otherCoords?.longitude || 0,
-    ) * 1000;
-
   if (!hasMounted) {
     return null;
   }
@@ -93,11 +59,9 @@ const InvitePage: NextPage = () => {
         position2={[otherCoords?.latitude || 0, otherCoords?.longitude || 0]}
       />
 
-      {(otherCoords || distanceBetweenPointsInMeters <= 15) && (
-        <Button onClick={() => onMint()} disabled={isLoading} loading={isLoading}>
-          Mint POM NFT
-        </Button>
-      )}
+      <Button onClick={() => onMint()} disabled={isLoading} loading={isLoading}>
+        Mint POM NFT
+      </Button>
 
       <div className="bg-warning text-primary-content rounded-md p-4 w-full">
         {/* <div className="break-words">
