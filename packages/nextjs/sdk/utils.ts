e03527878e52f502c316f41bce49aebec91cea71
mattiapomelli <mattiapomelli@gmail.com>
2023-10-08 09:39:31 +0200

    Fix

--- packages/nextjs/sdk/utils.ts
+++ packages/nextjs/sdk/utils.ts
@@ -3,13 +3,17 @@ import { ILocationMessage } from "./types";
 import { IDecodedMessage } from "@waku/interfaces";
 
 export const decodeMessage = (message: IDecodedMessage) => {
-  const json = locationMessage.decode(message.payload).toJSON();
-  return {
-    ...json,
-    message: JSON.parse(json.message),
-  };
+  try {
+    const json = locationMessage.decode(message.payload).toJSON();
+    return {
+      ...json,
+      message: JSON.parse(json.message),
+    };
+  } catch (error) {
+    return null;
+  }
 };
 
 export const decodeMessages = (messages: IDecodedMessage[]) => {
-  return messages.map(decodeMessage) as ILocationMessage[];
+  return messages.map(decodeMessage).filter(m => m !== null) as ILocationMessage[];
 };
