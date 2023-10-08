2e58525eaebbe997a2704eb34a01e7821059d5b8
mattiapomelli <mattiapomelli@gmail.com>
2023-10-08 05:09:16 +0200

    Make pwa

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,4 +1,8 @@
-// @ts-check
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: true,
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -15,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
