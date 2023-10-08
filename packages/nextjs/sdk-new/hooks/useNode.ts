c1e276d85346f9866da4765ece430ce55a8a2d6d
mattiapomelli <mattiapomelli@gmail.com>
2023-10-08 02:51:18 +0200

    Add node options

--- packages/nextjs/sdk-new/hooks/useNode.ts
+++ packages/nextjs/sdk-new/hooks/useNode.ts
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import { enrTree, wakuDnsDiscovery } from "@waku/dns-discovery";
+import { wakuPeerExchangeDiscovery } from "@waku/peer-exchange";
 import { createLightNode, waitForRemotePeer } from "@waku/sdk";
 
 export const useNode = () => {
@@ -6,7 +8,15 @@ export const useNode = () => {
     queryKey: ["waku-node"],
     queryFn: async () => {
       // Create and start a Light Node
-      const node = await createLightNode({ defaultBootstrap: true });
+      const node = await createLightNode({
+        defaultBootstrap: false,
+        libp2p: {
+          peerDiscovery: [
+            wakuDnsDiscovery([enrTree["PROD"]], { lightPush: 6, filter: 6, store: 6 }),
+            wakuPeerExchangeDiscovery(),
+          ],
+        },
+      });
       await node.start();
 
       // Wait for a successful peer connection
